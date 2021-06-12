783ea1700e4c39fa18371fc94b0aaac8eb731f9d
DatDoubleD <buonqa05@gmail.com>
2021-06-12 17:05:02 +0700

    learn js

--- lesson_3_DOM_Document_Object_Model/AccessElement.js
+++ lesson_3_DOM_Document_Object_Model/AccessElement.js
@@ -2,23 +2,23 @@
 // 1. truy xuat vao cac element
 // co the access through: ID, class, tag, css selector, HTML collection
 
-//return element
+//getElementById - return element
 var headingId = document.getElementById('heading')
 console.log(headingId)
 
-//return collection
+//getElementsByClassName return collection
 // 1 lement cung tra ve collection, can duyet vong lap de lay element
 var headingClass = document.getElementsByClassName('headingclass')
 console.log(headingClass)
 
-// return collection 
+//getElementsByTagName return collection 
 var headingTagName = document.getElementsByTagName('h1')
 console.log(headingTagName)
 
-//queryselector: return element, All: return collection
-// lay bang query with class="" them dau cham. dang truoc
-// var headingQuery = document.querySelector('.headingclass')
-//truong hop co 2 class trung ten nhau
+//querySelector - return element
+//querySelectorAll: return collection
+
+//truong hop co 2 class trung ten nhau, chon ten thu nhat hoac thu hai
 var headingQuery = document.querySelector(' .box .headingclass:nth-child(2)')
 console.log(headingQuery)
 
